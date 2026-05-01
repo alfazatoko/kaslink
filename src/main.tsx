f7cef1649ad179776959690c1647364ad5687365
KASLINK Admin <owner@kaslink.id>
2026-05-01 10:59:03 +0800

    Cleanup duplicates and disable SW

--- src/main.tsx
+++ src/main.tsx
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-console.log('main.tsx is running');
-try {
-  ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  )
-} catch (e) {
-  console.error('Render error:', e);
-  document.getElementById('root')!.innerHTML = `<h1 style="color:red">Fatal Error: ${e}</h1>`;
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
-// Service Worker Registration
+// Service Worker Registration (Disabled for debugging)
+/*
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch((err) => {
@@ -23,3 +18,4 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
+*/
